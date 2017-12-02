808bbb503c1426258e4e144cd5cfcd0f44804b31
papunapitam <xnovmir@gmail.com>
2017-12-02 18:03:10 +0100

    fix2

--- config/src/main/java/com/microsamples/config/ConfigApplication.java
+++ config/src/main/java/com/microsamples/config/ConfigApplication.java
@@ -2,13 +2,11 @@ package com.microsamples.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by xnovm on 18.06.2017.
  */
 @SpringBootApplication
-@EnableConfigServer
 public class ConfigApplication {
 
     public static void main(String[] args) {
