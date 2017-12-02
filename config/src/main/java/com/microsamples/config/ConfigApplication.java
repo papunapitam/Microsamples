47bb721aad82031ed3eea3f0a4205906feeb443b
papunapitam <xnovmir@gmail.com>
2017-12-02 18:19:38 +0100

    fix

--- config/src/main/java/com/microsamples/config/ConfigApplication.java
+++ config/src/main/java/com/microsamples/config/ConfigApplication.java
@@ -1,7 +1,14 @@
 package com.microsamples.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by xnovm on 18.06.2017.
@@ -11,5 +18,24 @@ public class ConfigApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
+    }
+}
+
+@RestController
+class HelloworldController {
+    private final String version = "1.0";
+
+    @RequestMapping("/hello/{name}")
+    public Map<String, String> hello(@Value("${greeting}") String greetingTemplate, @PathVariable String name) {
+        Map<String, String> response = new HashMap<String, String>();
+
+        String greeting = greetingTemplate
+                .replaceAll("\\$name", name)
+                .replaceAll("\\$version", version);
+
+        response.put("greeting", greeting);
+        response.put("version", version);
+
+        return response;
     }
 }
