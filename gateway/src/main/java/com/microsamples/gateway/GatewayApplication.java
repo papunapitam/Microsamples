e9838aebed013ce7bcaf03d7f516c89be584a53f
papunapitam <xnovmir@gmail.com>
2017-12-02 17:26:51 +0100

    test

--- gateway/src/main/java/com/microsamples/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microsamples/gateway/GatewayApplication.java
@@ -2,15 +2,11 @@ package com.microsamples.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by xnovm on 22.06.2017.
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
 public class GatewayApplication {
 
     public static void main(String[] args) {
